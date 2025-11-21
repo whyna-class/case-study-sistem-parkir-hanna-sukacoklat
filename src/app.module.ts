199f0b41d5e702122d1ea3bf5282e7a1e9659246
Gloria Hanna Prasetyo <gloria_prasetyo_33rpl@student.smktelkom-mlg.sch.id>
2025-11-21 09:13:32 +0700

    add resource park

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { ParkModule } from './park/park.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, ParkModule],
   controllers: [AppController],
   providers: [AppService],
 })
